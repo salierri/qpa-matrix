e08a29c196c69af1f417cdaecd3075840d1b752c
salierri <g.gergely314@gmail.com>
2015-07-07 00:49:38 +0200

    Changed route and additional error handling

--- routes/pixels.js
+++ routes/pixels.js
@@ -4,28 +4,32 @@ var router = express.Router();
 
 module.exports = function (dal, config) {
 
-    router.post('/get', function (req, res) {
-    	(function reservePixel() {
-	    	dal.Pixel.count({reserved: false}, function (err, doc) {
-	    		var number = Math.floor(Math.random() * doc);
-	    		dal.Pixel.find({reserved: false}).limit(-1).skip(number).exec(function (err, doc) {
-	    			dal.Pixel.findOneAndUpdate({_id: doc[0]._id, reserved: false}, {reserved: true}, function (err, doc) {
-	    				console.log(doc);
-	    				if(err) {
-	    					res.send(JSON.stringify({status: "error"}));
-	    				}
-						if(!doc) {
-							reservePixel();	
-							console.log("Race condition!");
-						} else {
-					    	new dal.User({hasPixel: true, pixel:{x: doc.x, y: doc.y}}).save(function (err, user) {
-					        	res.send(JSON.stringify({status: "reserved", pixel: {x: doc.x, y: doc.y}}));
-					    	})
-						}
-	    			});
-	    		});
-	    	});
-    	})();
+    router.get('/reserve', function (req, res) {
+        (function reservePixel() {
+            dal.Pixel.count({reserved: false}, function (err, doc) {
+                var number = Math.floor(Math.random() * doc);
+                dal.Pixel.find({reserved: false}).limit(-1).skip(number).exec(function (err, doc) {
+                    if(err | !doc) {
+                        console.log("Error during pixel randimization");
+                        res.send(JSON.stringify({status: "error"}));
+                    } else {
+                        dal.Pixel.findOneAndUpdate({_id: doc[0]._id, reserved: false}, {reserved: true}, function (err, doc) {
+                            if(err) {
+                                res.send(JSON.stringify({status: "error"}));
+                            }
+                            if(!doc) {
+                                reservePixel();    
+                                console.log("Race condition!");
+                            } else {
+                                new dal.User({hasPixel: true, pixel:{x: doc.x, y: doc.y}}).save(function (err, user)q {
+                                    res.send(JSON.stringify({status: "reserved", pixel: {x: doc.x, y: doc.y}}));
+                                })
+                            }
+                        });
+                    }
+                });
+            });
+        })();
     });
 
     return router;
